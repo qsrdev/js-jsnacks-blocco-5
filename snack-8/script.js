36769e2cc6dbc19ef7aacf0bce9dc02e712c51fc
qsrdev <matteo.albertini747@gmail.com>
2025-05-08 14:59:04 +0200

    task done

--- snack-8/script.js
+++ snack-8/script.js
@@ -22,5 +22,5 @@ const students = [
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
 
-let classe = students.find((student) => student.class);
+let classe = students.find((student) => student.name === "Marco Lanci");
 console.log(classe.class);
